8ffb6f4e6188752f4c74eb5eb56caa4ab2964d2e
Szczeżuja <szja@protonmail.com>
2022-06-07 19:15:50 +0200

    fingerclub.sh

---
+++ fingerclub.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Inspired on Lace by Drew/uoou/Friendo
+# gemini://friendo.monster/log/lace.gmi
+
+ESC=$(echo -e "\033")
+RED="[31m"
+CLUB=(warsaw@graph.no szczezuja@szczezuja.space alex@flounder.online phlog@1436.ninja)
+
+if [ ! -d "/tmp" ];then
+[[ ! -d "$HOME/.tmp" ]] && mkdir -p "$HOME/.tmp"
+	tmpfile=$(mktemp ~/.tmp/fingerclub.XXXXXX)
+else
+	tmpfile=$(mktemp /tmp/fingerclub.XXXXXX)
+fi
+
+for i in "${CLUB[@]}"
+do
+	echo -n $ESC$RED >> $tmpfile
+	echo "### $i$ESC" >> $tmpfile
+	finger $i >> $tmpfile
+	echo -e "\n\n" >> $tmpfile
+done
+
+less -RisW $tmpfile && rm -f $tmpfile
