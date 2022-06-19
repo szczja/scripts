de348f8092052ec9662621c01432209cb44d400b
Szczeżuja <szja@protonmail.com>
2022-06-19 15:27:30 +0200

    astrobotanyring.sh - comments and fixes

--- astrobotanyring.sh
+++ astrobotanyring.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Summary a water status of plants in Astrobotany plant ring
 
-PLANT_RING=(5d129edfe438478d8ec5a0e57c69a505 06206c2d3c1a408da14fcee89d04f87f dafd7db795e145dcbd052e5a83e869d7 d0263448b84d4238a21f8c450009172d)
+PLANT_RING=(
+	5d129edfe438478d8ec5a0e57c69a505
+	06206c2d3c1a408da14fcee89d04f87f
+	dafd7db795e145dcbd052e5a83e869d7
+	d0263448b84d4238a21f8c450009172d
+)
 
+# temp file
 if [ ! -d "/tmp" ];then
 [[ ! -d "$HOME/.tmp" ]] && mkdir -p "$HOME/.tmp"
         tmpfile=$(mktemp ~/.tmp/fingerclub.XXXXXX.gmi)
@@ -15,23 +21,29 @@ newline=false
 
 for i in "${PLANT_RING[@]}"
 do
+	# get url content
 	url="gemini://astrobotany.mozz.us/public/$i/m1"
-	output=$(openssl s_client -crlf -quiet -connect "astrobotany.mozz.us:1965" <<< $url)
+	output=$(openssl s_client -crlf -quiet -connect "astrobotany.mozz.us:1965" <<< $url 2>/dev/null)
 	if [ $newline = true ]
 	then
 		status+="\n"
 	fi
 	status+=$i
 	status+=":"
+	# regexp fit name
 	status+=$(echo -e $output | grep -Eo 'name : "(.)+"')
 	status+=":"
-	status+=$(echo -e $output | grep -Eo 'water(.)+%' | grep -Eo '[0-9]+%')
+	# regexp fit water and bonus, it should be fixed
+	status+=$(echo -e $output | grep -Eo 'water :(.)+{14}[0-9]+%' | grep -Eo '[0-9]+%')
 	newline=true
 done
 
-echo -en $status | awk -F ':' '{printf "=> gemini://astrobotany.mozz.us/public/%s/m1 %s %s\n",$1,$4,$3}' | sort -k 3 > $tmpfile
+# format output to gemtex, and sort by water status in column 3rd
+echo -en $status | awk -F ':' '{printf "=> gemini://astrobotany.mozz.us/app/visit/%s %s %s\n",$1,$4,$3}' | sort -k 3 > $tmpfile
 
+# header with probe time
 date=$(date)
 sed -i "1s/^/#Astrobotany ring - $date\n/" $tmpfile
 
+# amfora as gemini browser of choice
 amfora $tmpfile && rm -f $tmpfile
